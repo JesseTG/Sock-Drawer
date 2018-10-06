98c50f31d6f8a377a7db92cd63a64f8138358c20
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-10-06 12:26:41 -0400

    Remove some flask things from utils

--- sockpuppet/utils.py
+++ sockpuppet/utils.py
@@ -4,15 +4,5 @@
 from collections import defaultdict, namedtuple
 from typing import Dict, List, Sequence, Tuple
 
-from flask import flash
-
-
-def flash_errors(form, category='warning'):
-    """Flash all errors for a form."""
-    for field, errors in form.errors.items():
-        for error in errors:
-            flash('{0} - {1}'.format(getattr(form, field).label.text, error), category)
-
-
 NOT_BOT = 0
 BOT = 1
