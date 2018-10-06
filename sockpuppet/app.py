3eaff26bf4fdf66b19251216a093c1541b07a508
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-10-06 12:32:10 -0400

    Add type signatures in app.py

--- sockpuppet/app.py
+++ sockpuppet/app.py
@@ -7,7 +7,7 @@ from sockpuppet.extensions import api, cache, debug_toolbar
 from sockpuppet.settings import ProdConfig
 
 
-def create_app(config_object=ProdConfig):
+def create_app(config_object=ProdConfig) -> Flask:
     """An application factory, as explained here: http://flask.pocoo.org/docs/patterns/appfactories/.
 
     :param config_object: The configuration object to use.
@@ -22,7 +22,7 @@ def create_app(config_object=ProdConfig):
     return app
 
 
-def register_extensions(app):
+def register_extensions(app: Flask):
     """Register Flask extensions."""
     api.init_app(rest.poc.blueprint)
     cache.init_app(app)
@@ -31,29 +31,29 @@ def register_extensions(app):
     # TODO: Can I do this in a more idiomatic way?
     api.add_resource(rest.poc.ProofOfConcept, "/get")
 
-    return None
+def register_resources(app: Flask):
+    api.add_resource(v1.User, "/user")
 
 
-def register_blueprints(app):
+def register_blueprints(app: Flask):
     """Register Flask blueprints."""
     app.register_blueprint(public.views.blueprint)
     app.register_blueprint(rest.poc.blueprint, url_prefix="/api/0")
     return None
 
 
-def register_errorhandlers(app):
+def register_errorhandlers(app: Flask):
     """Register error handlers."""
-    def render_error(error):
+    def render_error(error: Exception):
         """Render error template."""
         # If a HTTPException, pull the `code` attribute; default to 500
         error_code = getattr(error, 'code', 500)
         return render_template('{0}.html'.format(error_code)), error_code
     for errcode in [401, 404, 500]:
         app.errorhandler(errcode)(render_error)
-    return None
 
 
-def register_shellcontext(app):
+def register_shellcontext(app: Flask):
     """Register shell context objects."""
     def shell_context():
         """Shell context objects."""
@@ -64,7 +64,7 @@ def register_shellcontext(app):
     app.shell_context_processor(shell_context)
 
 
-def register_commands(app):
+def register_commands(app: Flask):
     """Register Click commands."""
     app.cli.add_command(commands.test)
     app.cli.add_command(commands.lint)
