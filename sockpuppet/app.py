79914cb731f0b9bbf6b3baa5d60cd752af518cd0
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-10-06 12:35:03 -0400

    Simplify an error handler

--- sockpuppet/app.py
+++ sockpuppet/app.py
@@ -54,7 +54,7 @@ def register_errorhandlers(app: Flask):
         """Render error template."""
         # If a HTTPException, pull the `code` attribute; default to 500
         error_code = getattr(error, 'code', 500)
-        return render_template('{0}.html'.format(error_code)), error_code
+        return str(error), error_code
     for errcode in [401, 404, 500]:
         app.errorhandler(errcode)(render_error)
 
