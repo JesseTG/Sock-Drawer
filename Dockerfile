58977b60a0ae16f313eed75433797924f7fcb79c
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-10-12 22:19:50 -0400

    Provision the Dockerfile in a script

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,9 @@ ENV WORKDIR="${WORKDIR:-/app}"
 ENV FLASK_APP ${WORKDIR}/main.py
 ENV FLASK_DEBUG 0
 
-#RUN addgroup -S ${APP_USER} && adduser -S -G ${APP_USER} ${APP_USER}
-RUN apk update && apk add --no-cache gcc g++ libffi-dev
-#RUN apt-get update && apt-get install gcc g++ libffi-dev
-
 COPY . ${WORKDIR}
 
-RUN ${WORKDIR}/provision.sh
+# TODO: Open a UNIX socket for logging, and one for communication
+RUN "${WORKDIR}/provision.sh"
 
 EXPOSE 80 443
--- provision.sh
+++ provision.sh
@@ -1,3 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-pip3 install --no-cache-dir --requirement requirements/prod.txt
+# Add system-level dependencies
+apk update
+apk add --no-cache gcc g++ libffi-dev libxml2-dev libxslt-dev
+# TODO: Install libzmq
+
+# Add Python dependencies
+pip3 install -v --no-cache-dir --compile --requirement requirements/prod.txt
+
+# Remove unneeded packages
+apk del gcc g++ libffi-dev libxml2-dev libxslt-dev libc-utils
+# TODO: Look to see what other packages I can remove
+
+# Remove other cached data
+rm -rf /var/cache /tmp/*
+mkdir -p /var/cache
