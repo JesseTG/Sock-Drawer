a6ebf694eebf6d3c3c5cba748aa32822d7b2a981
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-10-26 14:01:07 -0400

    Remove flask_restful from extensions.py

--- sockpuppet/extensions.py
+++ sockpuppet/extensions.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 """Extensions module. Each extension is initialized in the app factory located in app.py."""
 from flask_caching import Cache
-from flask_restful import Api
 from flask_zmq import ZMQSocket
 
-api = Api()
 cache = Cache()
 zmq_socket = ZMQSocket()
