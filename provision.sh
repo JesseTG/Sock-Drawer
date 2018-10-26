9b25a912f13855d0944c78ca44260c91378a5fc5
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2018-10-26 14:02:41 -0400

    Other stuff
    - I'm hungry

--- provision.sh
+++ provision.sh
@@ -1,17 +1,38 @@
 #!/usr/bin/env sh
 
-# Add system-level dependencies
-apk update
-apk add --no-cache gcc g++ libffi-dev libxml2-dev libxslt-dev
-# TODO: Install libzmq
+set -e
+# Quit in error if any of these commands fails
 
-# Add Python dependencies
-pip3 install -v --no-cache-dir --compile --requirement requirements/prod.txt
+# # Add system-level dependencies
+# apk update
+# apk add --no-cache gcc g++ libffi-dev libxml2-dev libxslt-dev yaml-dev zeromq-dev
 
-# Remove unneeded packages
-apk del gcc g++ libffi-dev libxml2-dev libxslt-dev libc-utils
-# TODO: Look to see what other packages I can remove
+# # Add Python dependencies
+# pip3 install --no-cache-dir --compile --requirement requirements/prod.txt
 
-# Remove other cached data
-rm -rf /var/cache /tmp/*
-mkdir -p /var/cache
+# # Remove unneeded packages
+# apk del gcc g++ libffi-dev libxml2-dev libxslt-dev libc-utils yaml-dev
+# # Intentionally not removing zeromq-dev
+# # TODO: Look to see what other packages I can remove
+
+# # Remove other cached data
+# rm -rf /tmp/*
+
+
+apt-get update
+apt-get dist-upgrade --yes
+apt-get install --no-install-recommends --yes gcc libc-dev libffi-dev libxml2-dev libxslt-dev libyaml-dev libzmq5
+# Need gcc to compile some Python modules
+
+pip3.7 install --upgrade --no-cache-dir --compile --requirement requirements/prod.txt
+# Install the Python modules
+
+apt-get purge --yes -qq binutils gcc manpages libc-dev-bin *-dev
+apt-get autoremove --yes
+apt-get clean --yes
+apt-get autoclean --yes
+# Now we don't need gcc
+
+# rm -rf /var/log /var/backups /var/lib/apt /var/lib/dpkg
+# mkdir -p /var/log /var/backups /var/lib/apt /var/lib/dpkg
+# Clean the caches for good measure
